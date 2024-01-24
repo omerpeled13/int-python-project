c0349a822bd099b5f1ea39efe4a5b2d53e500d0a
omerpeled13 <138997776+omerpeled13@users.noreply.github.com>
2024-01-24 22:34:45 +0200

    python project

---
+++ main.py
@@ -0,0 +1,59 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+
+    Args:
+        message: received message (in order to get the user's name)
+    """
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward received message back to the sender
+
+    Args:
+        message: received message
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # handle wrong type error
+        await message.answer("נא הזן הודעה בפורמט תקין")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
